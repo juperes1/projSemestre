7df5b7d4ed4ca93058204e7b34e6dc2ae2fb404e
Aluno Dev24 <julia.peres0910@gmail.com>
2025-03-18 10:50:21 -0300

    projSemestre

--- api_semestre/api/serializers.py
+++ api_semestre/api/serializers.py
@@ -5,13 +5,29 @@ class CadastroFilmeSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.CadastroFilme
         fields = "__all__"
+        extra_kwargs={
+            'id': {'help_text': 'Indicador do cadastro de filme'},
+            'Nome': {'help_text': 'Nome do autor do filme cadastrado'},
+            'Imagem': {'help_text': 'Imagem representando a capa do filme'},
+            'NomeAutor': {'help_text': 'Nome do autor do filme cadaastrado'},
+        }
 
 class CadastroResenhaSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.CadastroResenha
         fields = "__all__"
+        extra_kwargs={
+            'id': {'help_text': 'Indicador do cadastro de resenha'},
+            'Comentário': {'help_text': 'Resenha - opinião sobre o filme'},
+        }
 
 class CadastroPerfilSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.CadastroPerfil
         fields = "__all__"
+        extra_kwargs={
+            'id':{'help_text': 'Indicador do cadastro de perfil'},
+            'Nome':{'help_text': 'Nome do usuário'},
+            'Foto':{'help_text': 'Foto de perfil do usuário'},
+            'Bio':{'help_text': 'Biografia com informações da preferência do suário'},
+        }
--- api_semestre/api/viewsets.py
+++ api_semestre/api/viewsets.py
@@ -1,15 +1,118 @@
 from rest_framework import viewsets
 from api_semestre.api import serializers
 from api_semestre import models
+from drf_yasg.utils import swagger_auto_schema
 
 class CadastroFilmeViewset(viewsets.ModelViewSet):
     serializer_class = serializers.CadastroFilmeSerializer
     queryset = models.CadastroFilme.objects.all()
+    @swagger_auto_schema(
+        operation_description="Cadastro de filme",
+        responses={200: serializers.CadastroFilmeSerializer(many=True)}
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+    
+    @swagger_auto_schema(
+        operation_description="Cria um novo cadastro de filme",
+        responses={201: "Cadastro de filme realizado com sucesso!"}
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+    
+    @swagger_auto_schema(
+        operation_description="Retorna o cadastro do filme conforme o ID informado",
+        responses={200: "Cadastro de filme encontrado com sucesso"}
+    )
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+    
+    @swagger_auto_schema(
+        operation_description="Altera o cadastro do filme conforme ID e dados informados",
+        responses={200: "Cadastro de filme alterado com sucesso!"}
+    )
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+    
+    @swagger_auto_schema(
+        operation_description="Deleta cadastro de filme conforme ID e dados informado",
+        responses={200: "Cadastro de filme deletado com sucesso"}
+    )
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
 
 class CadastroResenhaViewset(viewsets.ModelViewSet):
     serializer_class = serializers.CadastroResenhaSerializer
     queryset = models.CadastroResenha.objects.all()
+    @swagger_auto_schema(
+        operation_description="Cadastro de resenha",
+        responses={200: serializers.CadastroResenhaSerializer(many=True)}
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+    
+    @swagger_auto_schema(
+        operation_description="Cria um novo cadastro de resenha",
+        responses={201: "Cadastro de resenha realizado com sucesso!"}
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+    
+    @swagger_auto_schema(
+        operation_description="Retorna o cadastro da renha conforme o ID informado",
+        responses={200: "Cadastro da resenha encontrado com sucesso"}
+    )
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+    
+    @swagger_auto_schema(
+        operation_description="Altera o cadastro da resenha conforme ID e dados informados",
+        responses={200: "Cadastro de resenha alterado com sucesso!"}
+    )
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+    
+    @swagger_auto_schema(
+        operation_description="Deleta cadastro de resenha conforme ID e dados informado",
+        responses={200: "Cadastro de resenha deletado com sucesso"}
+    )
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
 
 class CadastroPerfilViewset(viewsets.ModelViewSet):
     serializer_class = serializers.CadastroPerfilSerializer
     queryset = models.CadastroPerfil.objects.all()
+    @swagger_auto_schema(
+        operation_description="Cadastro de perfil",
+        responses={200: serializers.CadastroPerfilSerializer(many=True)}
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+    
+    @swagger_auto_schema(
+        operation_description="Cria um novo cadastro de perfil",
+        responses={201: "Cadastro de perfil realizado com sucesso!"}
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+    
+    @swagger_auto_schema(
+        operation_description="Retorna o cadastro de perfil conforme o ID informado",
+        responses={200: "Cadastro de perfil encontrado com sucesso"}
+    )
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+    
+    @swagger_auto_schema(
+        operation_description="Altera o cadastro de perfil conforme ID e dados informados",
+        responses={200: "Cadastro de perfil alterado com sucesso!"}
+    )
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+    
+    @swagger_auto_schema(
+        operation_description="Deleta cadastro de perfil conforme ID e dados informado",
+        responses={200: "Cadastro de perfil deletado com sucesso"}
+    )
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
